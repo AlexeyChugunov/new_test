6237cd6910423f6b4d4175cbee9340b0f176cdc8
AlexeyChugunov <alex-chu@mail.ru>
2017-04-27 23:11:42 +0300

    Initial commit

---
+++ class1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexey
+ * Date: 27.04.2017
+ * Time: 23:10
+ */
---
+++ new_test.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Alexey
+ * Date: 27.04.2017
+ * Time: 23:10
+ */
+class new_test
+{
+spdofvnjs
+}
